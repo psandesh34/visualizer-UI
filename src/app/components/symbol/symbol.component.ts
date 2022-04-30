7f4edfce31d642c9625732e2c4c6420b2716b5d7
Sandesh <psandesh01.sp@gmail.com>
2022-04-30 23:47:16 +0530

    Added a table to view trades for a symbol

--- src/app/components/symbol/symbol.component.ts
+++ src/app/components/symbol/symbol.component.ts
@@ -1,8 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 declare const TradingView: any;
-
+export interface TradeInterface {
+  exchange: string,
+  price: number,
+  quantity: number,
+  tradeDate: Date,
+  tradeType: string
+}
 @Component({
   selector: 'app-symbol',
   templateUrl: './symbol.component.html',
@@ -25,9 +34,43 @@ export class SymbolComponent implements OnInit {
       industry: '',
     },
   };
+  trades: any = [];
+  dataSource: MatTableDataSource<TradeInterface>;
+  displayedColumns: string[] = [
+    'quantity',
+    'price',
+    'tradeType',
+    'exchange',
+    'tradeDate'
+  ];
+  chartData = undefined;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit(): void {
     this.symbolName = this.activatedRoute.snapshot.paramMap.get('symbolName');
     this.getSymbolDetails();
+    this.getSymbolTrades();
+  }
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadTableData() {
+    this.dataSource = new MatTableDataSource(this.trades);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   getSymbolDetails() {
@@ -36,5 +79,12 @@ export class SymbolComponent implements OnInit {
       .subscribe((data: any) => {
         this.data = data;
       });
+  }
+
+  getSymbolTrades() {
+    this.http.get('http://localhost:3000/trades/myUserId?symbol=' + this.symbolName).subscribe((data: any) => {
+      this.trades = data;
+      this.loadTableData();
+    });
   }
 }
